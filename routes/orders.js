f647f7cf57ff45916d3b4bf570340ca1899e88a5
Carlos F Arboleda G <arbofercho@gmail.com>
2021-07-04 11:38:19 -0500

    Change payment for inventory to give the example more sense

--- routes/orders.js
+++ routes/orders.js
@@ -3,12 +3,12 @@
 const { Router } = require("express");
 const router = Router();
 const orders = require('../src/orders');
-const payment = require('../src/payment');
+const inventory = require('../src/inventory');
 const delivery = require('../src/delivery');
 
 router.post('/', async (req, res) => {
     const orderId = await orders.createOrder(req.body);
-    await payment.makePaymenent(orderId);
+    await inventory.updateInventory(orderId);
     await delivery.scheduleDelivery(orderId);
     res.status(201).send({ orderId });
 });
